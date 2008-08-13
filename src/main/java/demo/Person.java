9e7d21b03afc0a117aa573b8cd9ea7342deab029
idarwin <ian>
2008-08-13 03:57:49 +0000

    First name and last name are fields now. getName() still exists, just composites firstname, lastname.

--- src/main/java/demo/Person.java
+++ src/main/java/demo/Person.java
@@ -1,19 +1,59 @@
 package demo;
 
-/**
- * Created by JBoss Tools
- */
 public class Person {
-	private String name;
+	private String firstName, lastName;
 
 	public Person() {
 	}
 
 	public String getName() {
-		return name;
+		return firstName + " " + lastName;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public int hashCode() {
+		final int PRIME = 31;
+		int result = 1;
+		result = PRIME * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = PRIME * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final Person other = (Person) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
 	}
 }
