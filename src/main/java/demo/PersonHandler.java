221af7201ec391698989d499507600fb921f3f49
idarwin <ian>
2011-06-22 18:05:29 +0000

    Add accessors.

--- src/main/java/demo/PersonHandler.java
+++ src/main/java/demo/PersonHandler.java
@@ -1,11 +1,22 @@
 package demo;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 
 @ManagedBean
 public class PersonDao {
-	public String savePerson(Person p) {
-		System.out.println("Saving person " + p.getFirstName() + ' ' + p.getLastName());
+	@ManagedProperty("person") Person person;
+
+	public String savePerson() {
+		System.out.println("Saving person " + person.getFirstName() + ' ' + person.getLastName());
 		return "saved";
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
 	}
 }
